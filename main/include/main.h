f91e7559f50b8d1328e383111e753d10804dae96
trigger1996 <529388771@qq.com>
2018-07-22 09:40:58 +0000

    branch first commit

--- main/include/main.h
+++ main/include/main.h
@@ -5,6 +5,13 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/time.h>
 
 #include <opencv2/opencv.hpp>
 
--- main/main.cpp
+++ main/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 using namespace cv;
 using namespace ORB_SLAM2;
 
+int serial_send( int fd, char *Data );
+
 namespace ORB_SLAM2 {
 
 class __myslam : public System {
@@ -23,9 +25,19 @@ public:
 protected:
 
 };
-};
+}
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    int fd;
+    int num;
+    struct termios oldstdio;
+    
+    float x, y, z;
+    float x_last, y_last, z_last;
+    float vx, vy, vz;
+    char tx[200] = { 0 };
+    char rx[200] = { 0 };
 
     VideoCapture cap(0);
     if (cap.isOpened() == false) {
@@ -35,14 +47,27 @@ int main(int argc, char **argv) {
 
     Mat frame;
     Mat f_left, f_right;
+    
+    double time = 0, dt = 0;
+    struct timeval t_now = { 0 }, t_last = { 0 };
+    
+    fd = open("/dev/ttyAMA0", O_RDWR );
+    if( -1==fd )
+    {
+        printf("cannot open /dev/ttyAMA0\r\n");
+        return -1;
+    }
+    tcgetattr( fd, &oldstdio);
+    cfsetispeed(&oldstdio, B115200);
+    tcsetattr( fd, TCSANOW, &oldstdio);
+    tcflush( fd, TCIFLUSH );
 
-    double time = 0;
-
+    
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::__myslam SLAM(argv[1],argv[2],ORB_SLAM2::System::STEREO,true);
+    ORB_SLAM2::__myslam SLAM(argv[1],argv[2],ORB_SLAM2::System::STEREO,true);       // true
 
     while (true) {
-
+    
         cap >> frame;
         //resize(frame, frame, Size(640, 480));
         //imshow("cam", frame);
@@ -50,25 +75,55 @@ int main(int argc, char **argv) {
         f_left = frame(Rect(320, 0, frame.cols - 320, frame.rows));     // 这个相机左右是反的
         //imshow("left", f_left);
         //imshow("right", f_right);
+        
+        t_last = t_now;
+        gettimeofday(&t_now, NULL);
+        dt = t_now.tv_usec - t_last.tv_usec;
+        if (dt < 0.)
+            dt += 1e6;
+        
+        time += dt;
+        Mat cam_center = SLAM.TrackStereo(f_left, f_right, time);
 
+        cout << "dt: " << dt << endl;
+        
+         //Mat cam_center = SLAM.get_Pos();
+        if (!cam_center.empty()) {
+            //cout << cam_center << endl;     // mm
+            x_last = x;
+            y_last = y;
+            z_last = z;
+            x = cam_center.at<float>(0, 3);
+            y = cam_center.at<float>(1, 3);
+            z = cam_center.at<float>(2, 3);
 
-        time+=5000;
-        SLAM.TrackStereo(f_left, f_right, time);
+            vx = (x - x_last) * 1e6 / dt;
+            vy = (y - y_last) * 1e6 / dt;
+            vz = (z - z_last) * 1e6 / dt;
 
-        Mat cam_center = SLAM.get_Pos();
-        if (!cam_center.empty())
-            cout << cam_center << endl;     // mm
+            cout << "x: " << x << " y: " << y << " z: " << z << endl;
 
-        if (waitKey(5) == 27) {
-            SLAM.Shutdown();
-            return 0;
+            memset(tx, 0, sizeof(char) * 120);
+            sprintf(tx, "x:%f y:%f z:%f vx:%f vy:%f vz:%f\r\n", x, y, z, vx, vy, vz);
+            num = serial_send(fd, tx);
         }
-
+//        if (waitKey(5) == 27) {
+//            SLAM.Shutdown();
+//            return 0;
+//        }
     }
 
     // Stop all threads
     SLAM.Shutdown();
-
-    waitKey(0);
+    close(fd);
+    
+    close(fd);
     return 0;
+}
+
+int serial_send( int fd, char *Data )
+{
+    int string_num;
+    string_num = strlen(Data);
+    return  write( fd,Data, string_num );
 }
